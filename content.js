7c3fcd3de126c36fceafb2f30d68412f9f03a205
Tyler Walker <twalker@erskine247.com>
2017-02-13 20:01:29 -0500

    Added main timeline parser

--- content.js
+++ content.js
@@ -1,1 +1,11 @@
-alert("We're in!")
+function talkingBody() { // Maybe scan ALL tweets on initial load, then just scan new tweet every interval
+	for (var i = 0; i < document.getElementsByClassName('js-tweet-text').length; i++) { //substring tweet length THE LAST INDEX OF THE TWEET SHOULD BE NULL
+		var tweetWords = document.getElementsByClassName('js-tweet-text')[i].innerHTML.split(" ");
+		for (var j = 0; j < tweetWords.length; j++) {
+			if (tweetWords[j].toLowerCase() == "me") {
+				document.getElementsByClassName('js-tweet-text')[i].innerHTML = "";
+			}
+		}
+	}
+}
+setInterval(function(){ talkingBody(); },3000);
