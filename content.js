d773dd7247556cb6424aca96b5f2b85b9c7d2b99
Tyler Walker <twalker@erskine247.com>
2017-02-14 20:34:19 -0500

    Added chrome's new storage protocol

--- content.js
+++ content.js
@@ -1,15 +1,17 @@
 // new name: perhaps tweetdeck filter?
-var blockedWords = ["hate", "go", "more"];
-function filter() {
-	for (var i = 0; i < document.getElementsByClassName('js-tweet-text').length; i++) {
-		var tweetWords = document.getElementsByClassName('js-tweet-text')[i].innerHTML.split(" ");
-		for (var j = 0; j < tweetWords.length; j++) {
-			for (var k = 0; k < blockedWords.length; k++) {
-				if (tweetWords[j].toLowerCase() == blockedWords[k]) {
-					document.getElementsByClassName('js-tweet-text')[i].parentNode.parentNode.parentNode.parentNode.innerHTML = "";
+chrome.storage.sync.get(["blockedWords"], function(items) {
+	var blockedWords = items.blockedWords.split(",");
+	function filter() {
+		for (var i = 0; i < document.getElementsByClassName('js-tweet-text').length; i++) {
+			var tweetWords = document.getElementsByClassName('js-tweet-text')[i].innerHTML.split(" ");
+			for (var j = 0; j < tweetWords.length; j++) {
+				for (var k = 0; k < blockedWords.length; k++) {
+					if (tweetWords[j].toLowerCase() == blockedWords[k].trim().toLowerCase()) {
+						document.getElementsByClassName('js-tweet-text')[i].parentNode.parentNode.parentNode.parentNode.innerHTML = "";
+					}
 				}
 			}
 		}
 	}
-}
-setInterval(function(){ filter(); }, 3000);
+	setInterval(function(){ filter(); }, 3000);
+});
